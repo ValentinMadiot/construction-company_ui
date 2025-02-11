0bddd2c74d4f79886aebd27c5a09478b394a419b
valentinmadiot <valentin.madiot@gmail.com>
2025-02-11 18:50:47 +0700

    add services

--- assets/js/main.js
+++ assets/js/main.js
@@ -42,7 +42,17 @@ window.addEventListener("scroll", bgHeader);
 bgHeader()
 
 /*=============== SWIPER SERVICES ===============*/ 
+const swiperServices = new Swiper('.services__swiper', {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 24,
+  slidesPerView: 'auto',
 
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+})
 
 /*=============== SHOW SCROLL UP ===============*/ 
 
