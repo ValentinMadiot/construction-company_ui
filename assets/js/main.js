df3bae92ebd6d1f1e254e75b05ec5b1c2545aeb8
valentinmadiot <valentin.madiot@gmail.com>
2025-02-11 20:21:07 +0700

    add scroll up button

--- assets/js/main.js
+++ assets/js/main.js
@@ -55,7 +55,15 @@ const swiperServices = new Swiper('.services__swiper', {
 })
 
 /*=============== SHOW SCROLL UP ===============*/ 
+const scrollUp = () => {
+  const scrollUpElement = document.getElementById("scroll-up");
+  // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+  window.scrollY >= 350
+    ? scrollUpElement.classList.add("show-scroll")
+    : scrollUpElement.classList.remove("show-scroll");
+};
 
+window.addEventListener("scroll", scrollUp);
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
